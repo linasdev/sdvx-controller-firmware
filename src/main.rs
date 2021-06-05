848069e8ebc541b2f4158000529b5ef0f12b3341
Linas Nikiperavičius <linas@linasdev.com>
2021-06-05 18:33:44 +0300

    Send keyboard reports for a single button

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@
 use panic_semihosting as _;
 
 use cortex_m_rt::entry;
-use stm32f1xx_hal::prelude::*;
+use embedded_hal::digital::v2::InputPin;
+use stm32f1xx_hal::{prelude::*, gpio};
 use stm32f1xx_hal::usb::{Peripheral, UsbBus, UsbBusType};
 use stm32f1xx_hal::stm32::{interrupt, Interrupt};
 use stm32f1xx_hal::pac::{Peripherals, NVIC};
+use stm32f1xx_hal::gpio::{ExtiPin, Edge, Input, PullDown};
 use usb_device::prelude::*;
 use usb_device::bus::UsbBusAllocator;
 use usbd_hid::descriptor::generator_prelude::*;
@@ -17,6 +19,9 @@ use usbd_hid::hid_class::HIDClass;
 static mut USB_BUS: Option<UsbBusAllocator<UsbBusType>> = None;
 static mut USB_HID: Option<HIDClass<UsbBusType>> = None;
 static mut USB_DEV: Option<UsbDevice<UsbBusType>> = None;
+
+static mut INPUT: Option<gpio::gpioa::PA7<Input<PullDown>>> = None;
+static mut STATE: bool = false;
 
 #[entry]
 fn main() -> ! {
@@ -36,7 +41,17 @@ fn main() -> ! {
 
     assert!(clocks.usbclk_valid());
 
-    let gpioa = dp.GPIOA.split(&mut rcc.apb2);
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+    let mut input = gpioa.pa7.into_pull_down_input(&mut gpioa.crl);
+    input.make_interrupt_source(&mut afio);
+    input.trigger_on_edge(&dp.EXTI, Edge::RISING_FALLING);
+    input.enable_interrupt(&dp.EXTI);
+
+    unsafe {
+        INPUT = Some(input);
+    }
 
     let usb = Peripheral {
         usb: dp.USB,
@@ -69,9 +84,45 @@ fn main() -> ! {
     unsafe {
         NVIC::unmask(Interrupt::USB_HP_CAN_TX);
         NVIC::unmask(Interrupt::USB_LP_CAN_RX0);
+        NVIC::unmask(Interrupt::EXTI9_5);
     }
 
+    let usb_hid = unsafe { USB_HID.as_ref().unwrap() };
+
+    let report1 = KeyboardReport {
+        modifier: 0x00,
+        leds: 0x00,
+        keycodes: [
+            0x30,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+        ],
+    };
+
+    let report2 = KeyboardReport {
+        modifier: 0x00,
+        leds: 0x00,
+        keycodes: [
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+            0x00,
+        ],
+    };
+
+    let state = unsafe { &STATE };
+
     loop {
+        if *state {
+            usb_hid.push_input(&report1);
+        } else {
+            usb_hid.push_input(&report2);
+        }
     }
 }
 
@@ -80,6 +131,23 @@ fn poll_usb() {
     let usb_dev = unsafe { USB_DEV.as_mut().unwrap() };
 
     usb_dev.poll(&mut [usb_hid]);
+}
+
+#[interrupt]
+fn EXTI9_5() {
+    let input = unsafe { INPUT.as_mut().unwrap() };
+
+    if input.check_interrupt() {
+        let state = unsafe { &mut STATE };
+
+        if input.is_high().unwrap() {
+            *state = true;
+        } else {
+            *state = false;
+        }
+
+        input.clear_interrupt_pending_bit();
+    }
 }
 
 #[interrupt]
